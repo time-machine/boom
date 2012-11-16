61e921a9427cddb503ecd4f532eb5a5ff13145ef
kumabotz <kumabotz@gmail.com>
2012-11-16 14:24:27 +0800

    Fix dash options under 1.9

--- lib/boom/command.rb
+++ lib/boom/command.rb
@@ -70,7 +70,7 @@ module Boom
         return all  if command == 'all'
         return edit if command == 'edit'
         return help if command == 'help'
-        return help if command[0] == '-' # any - dash options are please for help
+        return help if command[0] == 45 || command[0] == '-' # any - dash options are please for help
 
         # if we're operating on a List
         if storage.list_exists?(command)
