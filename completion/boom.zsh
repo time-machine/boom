c7c28cb08b29016298318ef751f7faef219750f1
kumabotz <kumabotz@gmail.com>
2012-11-21 10:10:29 +0800

    ZSH completion function added

--- completion/boom.zsh
+++ completion/boom.zsh
@@ -1,18 +1,29 @@
-# oh hai!
+#compdef boom
 
-# someone should fork this and add autocompletion for zsh
+local state line cmds ret=1
 
-# you'd be pretty cool
+_arguments -C '1: :->cmds' '*: :->args'
 
-# and I'd give you a high-five
+case $state in
+  cmds)
+    local -a cmds
+    cmds=(
+      'all:show all items in all lists'
+      'edit:edit the boom JSON file in $EDITOR'
+      'help:help text'
+    )
+    _describe -t commands 'boom command' cmds && ret=0
+    _values 'lists' $(boom | awk '{print $1}')
+    ;;
+  args)
+    case $line[1] in
+      (boom|all|edit|help)
+        ;;
+      *)
+        _values 'items' `boom $line[1] | awk '{print $1}' | sed -e 's/://'` 2>/dev/null && ret=0
+        ;;
+    esac
+    ;;
+esac
 
-
-
-# and maybe a whiskey
-
-
-
-
-
-
-# a couple of whiskeys
+return ret
