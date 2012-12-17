75990ec100f92ac73651ddfdc6ad6c011017d158
kumabotz <kumabotz@gmail.com>
2012-12-17 16:47:28 +0800

    Accept 'user', 'password', or 'uri' for redis connection

--- lib/boom/storage/redis.rb
+++ lib/boom/storage/redis.rb
@@ -4,6 +4,7 @@
 begin
   require 'digest'
   require 'redis'
+  require 'uri'
 rescue LoadError
 end
 
@@ -11,8 +12,11 @@ module Boom
   module Storage
     class Redis < Base
       def redis
-        @redis ||= ::Redis.new :host => Boom.config.attributes['redis']['host'],
-                               :port => Boom.config.attributes['redis']['port']
+        uri = URI.parse(Boom.config.attributes['uri'] || '')
+        @redis ||= ::Redis.new :host => (uri.host         || Boom.config.attributes['redis']['host']),
+                               :port => (uri.port         || Boom.config.attributes['redis']['port']),
+                               :user => (uri.user         || Boom.config.attributes['redis']['user']),
+                               :password => (uri.password || Boom.config.attributes['redis']['password'])
       rescue NameError => e
         puts "You don't have Redis installed yet:\n  gem install redis"
         exit
